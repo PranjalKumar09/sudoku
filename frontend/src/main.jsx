561411e1f282a0172101357a60d673a4a4ae5a87
pranjalkumar09 <coderkumarshukla@gmail.com>
2025-12-14 13:01:24 +0530

    Adjust frontend entry points and bootstrap logic for app initialization

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { HelmetProvider } from "react-helmet-async";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+window.adsbygoogle = window.adsbygoogle || [];
+window.adsbygoogle.push({});
+
+
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <HelmetProvider>
+      <StrictMode>
+        <App />
+      </StrictMode>
+      
+    </HelmetProvider>
+  </BrowserRouter>
+);
