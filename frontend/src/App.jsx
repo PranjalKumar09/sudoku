2c2a669ea79c4f1accdd34f15ef819e0c0182412
pranjalkumar09 <coderkumarshukla@gmail.com>
2026-01-04 12:26:32 +0530

    Implement root React component and application layout

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import GamePage from "./pages/GamePage";
+
+export default function App() {
+  return (
+    <div className="min-h-screen 3bg-gray-100 flex items-center justify-center">
+      <GamePage />
+    </div>
+  );
+}
